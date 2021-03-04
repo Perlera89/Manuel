055f00df2011babc5ef5754ee6541f73905cffe6
Perlera89 <69605277+Perlera89@users.noreply.github.com>
2021-03-04 14:32:22 -0600

    Agregando proyecto con conexion a ADO .NET

--- Manuel02/Manuel02/Program.cs
+++ Manuel02/Manuel02/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Manuel02.Vistas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Manuel02
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu());
+            Application.Run(new frmEntidades());
         }
     }
 }
