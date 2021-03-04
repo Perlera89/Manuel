b3b7aef4ca9bb96d932775353f0aaa4671cf6c00
Perlera89 <69605277+Perlera89@users.noreply.github.com>
2021-03-04 14:33:28 -0600

    Cambio de nombre a la carpeta

---
+++ Manuel03/Manuel02/Dominio/Entidades.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manuel02.Dominio
+{
+    class Entidades
+    {
+        string user;
+        string pass;
+
+        public string User { get => user; set => user = value; }
+        public string Pass { get => pass; set => pass = value; }
+    }
+}
---
+++ Manuel03/Manuel02/Negocio/ClsLogin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Manuel02.DAO;
+using Manuel02.Dominio;
+
+namespace Manuel02.Negocio
+{
+    class ClsLogin
+    {
+        ClsListaUsuario cls = new ClsListaUsuario();
+
+        //Metodo entrar() compara el valor del User y Pass introducidos en sus respectivos TextBox.
+        public int accesso(Entidades entidad)
+        {
+            int estado = 0;
+
+            //copiando y almacenando los elementos del array
+            cls.Users = cls.Users.Distinct().ToArray();
+            cls.Passwords = cls.Passwords.Distinct().ToArray();
+
+            //recorriendo arreglo para comparar
+            for(int i = 0; i < cls.Users.Length; i++)
+            {
+                if(entidad.User.Equals(cls.Users[i]) && entidad.Pass.Equals(cls.Passwords[i]))
+                {
+                    estado = 1;
+                }
+            }
+
+            return estado;
+        }
+    }
+}
