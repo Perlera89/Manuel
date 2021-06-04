77c11f064a26c061ba71e10111d40fb55ca98a8e
Manuel <69605277+Perlera89@users.noreply.github.com>
2021-06-04 14:14:36 -0600

    agregando proyecto finalizado de appVentas

---
+++ Historial/appVentas2/appVentas/DAO/ClsDDetalle.cs
@@ -0,0 +1,27 @@
+﻿using appVentas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appVentas.DAO
+{
+    class ClsDDetalle
+    {
+        public void GuardarDetalleVenta(tb_detalleVenta detalle)
+        {
+            using (sistema_ventasEntities bd = new sistema_ventasEntities())
+            {
+                tb_detalleVenta tbDetalle = new tb_detalleVenta();
+                tbDetalle.iDDetalleVenta = detalle.iDDetalleVenta;
+                tbDetalle.iDProducto = detalle.iDProducto;
+                tbDetalle.cantidad = detalle.cantidad;
+                tbDetalle.precio = detalle.precio;
+                tbDetalle.total = detalle.total;
+
+                bd.SaveChanges();
+            }
+        }
+    }
+}
---
+++ appVentas/appVentas/DAO/ClsVenta.cs
@@ -0,0 +1,41 @@
+﻿using appVentas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appVentas.DAO
+{
+    class ClsVenta
+    {
+        public List<tb_venta> UltimaVenta()
+        {
+            List<tb_venta> ConsultaUltimaVenta = new List<tb_venta>();
+
+            using (sistema_ventasEntities bd = new sistema_ventasEntities())
+            {
+                ConsultaUltimaVenta = bd.tb_venta.ToList();
+            }
+
+            return ConsultaUltimaVenta;
+        }
+
+        public void GuardarVenta(tb_venta ventas)
+        {
+            using (sistema_ventasEntities bd = new sistema_ventasEntities())
+            {
+                tb_venta venta = new tb_venta();
+
+                venta.iDDocumento = ventas.iDDocumento;
+                venta.iDCliente = ventas.iDCliente;
+                venta.iDUsuario = ventas.iDUsuario;
+                venta.totalVenta = ventas.totalVenta;
+                venta.fecha = ventas.fecha;
+                bd.tb_venta.Add(venta);
+                bd.SaveChanges();
+            }
+        }
+        
+    }
+}
